cb3954974040da9207a7c2560153c879de9f1e00
Pier Giulio Minetti <pg.minettigmail.com>
2025-10-01 15:48:15 +0200

    added inputs to test

--- src/app/components/inputs-component/inputs-component.ts
+++ src/app/components/inputs-component/inputs-component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ItAutocompleteComponent, ItCheckboxComponent } from 'design-angular-kit';
+import { ItAutocompleteComponent, ItCheckboxComponent, ItInputComponent, InputControlType, ItPasswordInputComponent, ItRadioButtonComponent } from 'design-angular-kit';
 
 @Component({
   selector: 'app-inputs-component',
-  imports: [ItAutocompleteComponent, FormsModule, ItCheckboxComponent],
+  imports: [ItAutocompleteComponent, FormsModule, ItCheckboxComponent, ItInputComponent, ItPasswordInputComponent, ItRadioButtonComponent],
   templateUrl: './inputs-component.html',
   styleUrl: './inputs-component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -37,4 +37,28 @@ export class InputsComponent {
   disabled = false;
   indeterminate = false;
   link = false;
+
+   i = 0;
+  readOnly = false;
+  type: InputControlType | 'password' = 'text';
+  icon = 'pencil';
+  value = 'myNgModel';
+
+  get placeholder() {
+    return this.hasPlaceholder ? 'Testo di esempio' : '';
+  }
+
+  get label() {
+    return this.hasLabel ? "Label dell'input" : '';
+  }
+
+  get note() {
+    return this.hasNote ? 'non condivideremo mai i tuoi dati' : '';
+  }
+
+  hasPlaceholder = true;
+
+  hasLabel = true;
+
+  hasNote = false;
 }
